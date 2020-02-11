2b0e79203a9cf3fba1f7815b1ebbe7632599b548
KY-Ng <raionkurane@gmail.com>
2020-02-11 11:19:49 +0900

    Implemented Line Graph Drawing.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const ctx = document.getElementById('chart').getContext('2d');
+
 class QueueModel {
   constructor(model_params) {
     this.numCounter = model_params.numCounter;
@@ -6,68 +8,90 @@ class QueueModel {
   }
 }
 
-class Counter {
-  constructor() {}
-}
-
 function createModel(params) {
   console.log("Creating Model...");
   // Create Model
   return new QueueModel(params);
 }
 
-function simulate(model, numSteps) {
-
-  // Initialize Counters
-  let counters = []
-  for (let n = 0; n < model.numCounter; n++) {
-    counters[n] = new Counter();
-  }
-
+function simulate(model, numSteps, delay) {
   console.log("Start Simulation...");
+  console.log(delay);
   // Start Simulation
   // Variables for Graph plotting
   let x = [0];
   let numCustomer = [0];
   let queueLength = [0];
+  // Show initial graph
+  let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: x,
+      datasets: [{
+        label: 'Number of Customer(s)',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        fill: false,
+        data: numCustomer
+      },
+      {
+        label: 'Length of Queue',
+        backgroundColor: 'rgb(99, 200, 132)',
+        borderColor: 'rgb(99, 200, 132)',
+        fill: false,
+        data: queueLength
+      }]
+    },
+    options: {
+      animation: {
+            duration: 0 // general animation time
+        }
+    }
+  })
+
   // Varibles
   let counterInUse = 0;
 
   for (let idx = 1; idx <= numSteps; idx++) {
-    console.log(idx, numCustomer[idx - 1], queueLength[idx - 1]);
-    // get previous number of customer and number of counters in use
-    let nextCustomerNum = numCustomer[idx - 1];
-    let currentCounterInUse = counterInUse;
-    // loop through each counter and see if they finished providing service
-    for (let c = 0; c < currentCounterInUse; c++) {
-      // provide service
-      if (Math.random() < model.mu) {
-        nextCustomerNum--;
-        counterInUse--;
+    setTimeout(() => {
+      // get previous number of customer and number of counters in use
+      let nextCustomerNum = numCustomer[idx - 1];
+      let currentCounterInUse = counterInUse;
+      // loop through each counter and see if they finished providing service
+      for (let c = 0; c < currentCounterInUse; c++) {
+        // provide service
+        if (Math.random() < model.mu) {
+          nextCustomerNum--;
+          counterInUse--;
+        }
       }
-    }
-    // add new customer
-    if (Math.random() < model.lambda) {
-      nextCustomerNum++;
-      // counter is empty
-      if (counterInUse < model.numCounter) {
-        counterInUse++;
-      }
-    }
-    // assign customer to counters
-    if (counterInUse === 0) {
-      for (let c = 0; c < model.numCounter; c++) {
-        // customers are waiting, so assign
-        if (nextCustomerNum > counterInUse) {
+      // add new customer
+      if (Math.random() < model.lambda) {
+        nextCustomerNum++;
+        // counter is empty
+        if (counterInUse < model.numCounter) {
           counterInUse++;
         }
       }
-    }
-    // update x
-    x.push(idx)
-    // update numCustomer & queueLength
-    numCustomer.push(nextCustomerNum);
-    queueLength.push(nextCustomerNum - counterInUse);
-    // update graph
+      // assign customer to counters
+      if (counterInUse === 0) {
+        for (let c = 0; c < model.numCounter; c++) {
+          // customers are waiting, so assign
+          if (nextCustomerNum > counterInUse) {
+            counterInUse++;
+          }
+        }
+      }
+      // update x
+      x.push(idx)
+      // update numCustomer & queueLength
+      numCustomer.push(nextCustomerNum);
+      queueLength.push(nextCustomerNum - counterInUse);
+      // update graph
+      chart.data.labels = x;
+      chart.data.datasets[0].data = numCustomer;
+      chart.data.datasets[1].data = queueLength;
+      chart.update();
+    }, delay*idx);
   }
 }
--- js/listener.js
+++ js/listener.js
@@ -1,8 +1,9 @@
-const numCounterInput = document.getElementById('numCounter')
-const lambdaInput = document.getElementById('lambda')
-const muInput = document.getElementById('mu')
-const stepsInput = document.getElementById('steps')
-const startButton = document.getElementById('start_btn')
+const numCounterInput = document.getElementById('numCounter');
+const lambdaInput = document.getElementById('lambda');
+const muInput = document.getElementById('mu');
+const stepsInput = document.getElementById('steps');
+const speedInput = document.getElementById('speed');
+const startButton = document.getElementById('start_btn');
 
 startButton.addEventListener('click', () => {
   console.log('clicked');
@@ -11,25 +12,5 @@ startButton.addEventListener('click', () => {
     lambda: parseFloat(lambdaInput.value),
     mu: parseFloat(muInput.value)
   })
-  simulate(model, parseInt(stepsInput.value));
+  simulate(model, parseInt(stepsInput.value), parseInt(speedInput.value));
 })
-
-// const ctx = document.getElementById('chart').getContext('2d');
-// let chart = new Chart(ctx, {
-//     // The type of chart we want to create
-//     type: 'line',
-//
-//     // The data for our dataset
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     },
-//
-//     // Configuration options go here
-//     options: {}
-// });
