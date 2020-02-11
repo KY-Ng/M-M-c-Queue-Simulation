473898d3d8db39b73ecba26f9814d22a9d36d789
KY-Ng <raionkurane@gmail.com>
2020-02-11 18:40:19 +0900

    Allow simulations that diverge too (unable to simulate previously because I used the wrong method <probability calculation>)

--- js/listener.js
+++ js/listener.js
@@ -4,7 +4,6 @@ const muInput = document.getElementById('mu');
 const initialCustomerInput = document.getElementById('initial_customer');
 const stepsInput = document.getElementById('steps');
 const speedInput = document.getElementById('speed');
-const conditionText = document.getElementById('conditionText');
 const startButton = document.getElementById('start_btn');
 const resetButton = document.getElementById('reset_btn');
 
@@ -13,22 +12,16 @@ startButton.addEventListener('click', () => {
   let lambda = parseFloat(lambdaInput.value);
   let mu = parseFloat(muInput.value);
 
-  if ((1 - (lambda/(numCounter*mu))) < 0) {
-    console.error("Invalid Paramters");
-    conditionText.style.color = "#F00";
-  } else {
-    conditionText.style.color = "#000";
-    let model = createModel({
-      numCounter: numCounter,
-      lambda: lambda,
-      mu: mu
-    })
+  let model = createModel({
+    numCounter: numCounter,
+    lambda: lambda,
+    mu: mu
+  })
 
-    let initialCustomer = parseInt(initialCustomerInput.value);
-    let steps = parseInt(stepsInput.value);
-    let speed = parseInt(speedInput.value);
-    simulate(model, initialCustomer, steps, speed);
-  }
+  let initialCustomer = parseInt(initialCustomerInput.value);
+  let steps = parseInt(stepsInput.value);
+  let speed = parseInt(speedInput.value);
+  simulate(model, initialCustomer, steps, speed);
 });
 
 resetButton.addEventListener('click', () => {
