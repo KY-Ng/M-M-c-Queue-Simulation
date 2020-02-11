779054f86ed2334e966d7170b4510177636f3532
KY-Ng <raionkurane@gmail.com>
2020-02-11 10:39:55 +0900

    Implemented data points generation loop (simulation loop)

--- js/app.js
+++ js/app.js
@@ -26,9 +26,48 @@ function simulate(model, numSteps) {
 
   console.log("Start Simulation...");
   // Start Simulation
-  for (let i = 0; i < numSteps; i++) {
-    console.log(i);
+  // Variables for Graph plotting
+  let x = [0];
+  let numCustomer = [0];
+  let queueLength = [0];
+  // Varibles
+  let counterInUse = 0;
+
+  for (let idx = 1; idx <= numSteps; idx++) {
+    console.log(idx, numCustomer[idx - 1], queueLength[idx - 1]);
+    // get previous number of customer and number of counters in use
+    let nextCustomerNum = numCustomer[idx - 1];
+    let currentCounterInUse = counterInUse;
+    // loop through each counter and see if they finished providing service
+    for (let c = 0; c < currentCounterInUse; c++) {
+      // provide service
+      if (Math.random() < model.mu) {
+        nextCustomerNum--;
+        counterInUse--;
+      }
+    }
     // add new customer
-    // provide service
+    if (Math.random() < model.lambda) {
+      nextCustomerNum++;
+      // counter is empty
+      if (counterInUse < model.numCounter) {
+        counterInUse++;
+      }
+    }
+    // assign customer to counters
+    if (counterInUse === 0) {
+      for (let c = 0; c < model.numCounter; c++) {
+        // customers are waiting, so assign
+        if (nextCustomerNum > 0) {
+          counterInUse++;
+        }
+      }
+    }
+    // update x
+    x.push(idx)
+    // update numCustomer & queueLength
+    numCustomer.push(nextCustomerNum);
+    queueLength.push(nextCustomerNum - counterInUse);
+    // update graph
   }
 }
